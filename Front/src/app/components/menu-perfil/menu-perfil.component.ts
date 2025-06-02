d7cf1c08048e630b885ea13da546d20ca2e20a04
Carla <carlabm1703@gmail.com>
2025-06-02 20:41:23 +0200

    error barra desplazamiento

--- Front/src/app/components/menu-perfil/menu-perfil.component.ts
+++ Front/src/app/components/menu-perfil/menu-perfil.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Usuario } from '../../interfaces/usuario';
 import { UsuarioService } from '../../services/usuario/usuario.service';
+declare module 'bootstrap'
+import { Offcanvas } from 'bootstrap';
+
 
 @Component({
   selector: 'app-menu-perfil',
@@ -32,12 +35,15 @@ export class MenuPerfilComponent {
   }
 
 
-
+  @ViewChild('DesplegableMenuPerfil') offcanvasElement!: ElementRef;
+  
   cerrarSesion(){
     this.usuarioService.cerrarSesion().subscribe({
       next: (res) => {
         this.toastr.success('Has cerrado sesión con éxito', undefined, {timeOut: 8000, closeButton: true});
-        this.router.navigate(['/']);
+        document.body.classList.remove('offcanvas-backdrop', 'offcanvas-open', 'modal-open');
+        document.body.style.overflow = 'auto';
+        this.router.navigate(['/']);  
       },
       error: (e) => {
         this.toastr.error('Error', e.message, {timeOut: 8000, closeButton: true});
